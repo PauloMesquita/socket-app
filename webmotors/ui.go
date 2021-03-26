fdbf44adacf4015d318ef849913d2473b0b58aa8
PauloMesquita <p.mesquita@vp6.com.br>
2021-03-26 09:49:46 -0300

    add all

--- webmotors/ui.go
+++ webmotors/ui.go
@@ -1,5 +1,11 @@
 package webmotors
 
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
 /*
 *1Menu:
 1_ Anunciar carro
@@ -28,12 +34,150 @@ Selecione o id do carro para comprar
 */
 
 type Ui struct {
-	step uint8
+	ClientStep map[string]uint8
+	WebmotorsApp *Webmotors
 }
 
-func (u *Ui) receive(message string) string {
-	if u.step == 0 {
-		return "Menu\n1_Anunciar carro\n2_Listar ofertas disponiveis\n3_Listar anuncios próprios\n4_Retirar anuncio próprio\n5_Comprar Carro\nDigite uma opção: "
+func (u *Ui) Receive(message string, name string) string {
+	if _, exist := u.ClientStep[name]; !exist {
+		u.ClientStep[name] = 0;
+	}
+	fmt.Printf("[UI Receive] Message received %s in step %d\n", message, u.ClientStep[name])
+	menuMessage := "\n### Menu ###\n1_Anunciar carro\n2_Listar ofertas disponiveis\n3_Listar anuncios próprios\n4_Retirar anuncio próprio\n5_Comprar Carro\n6_Listar meus carros\nDigite uma opção: "
+	// Menu
+	if u.ClientStep[name] == 0{
+		// Get message
+		code, err := strconv.ParseInt(message, 10, 8)
+		// User sent an option
+		if err == nil && code > 0 && code < 7 {
+			u.ClientStep[name] = uint8(code)
+		} else {
+			return menuMessage
+		}
+	}
+	// Anunciar Carro (perguntar informacao)
+	if(u.ClientStep[name] == 1){
+		u.ClientStep[name] = 7
+		return "Digite o modelo,cor,ano,quilometragem e preco, separados por ponto e virgula (ex: 'Ford Ka;Branco;1999;200000;35000,00') \n> "
+	}
+	// Listar ofertas disponíveis
+	if(u.ClientStep[name] == 2){
+		cars := u.WebmotorsApp.Cars
+		response := ""
+		for _, v := range cars {
+			if v.buyer == "" {
+				response = response + "\n" + StringfyCar(v)
+			}
+		}
+		u.ClientStep[name] = 0 // Return to menu step
+		if len(response) == 0 {
+			response = "\nNenhum carro a venda\n"
+		}
+		return response + menuMessage
+		
+	}
+	// Listar anuncios próprios
+	if(u.ClientStep[name] == 3){
+		cars := u.WebmotorsApp.ListOwnerCarsToSell(name)
+		response := ""
+		for _, v := range cars {
+			response = response + "\n" + StringfyCar(v)
+		}
+		u.ClientStep[name] = 0 // Return to menu step
+		if len(response) == 0 {
+			response = "\nNenhum carro a venda\n"
+		}
+		return response + menuMessage
+
+	}
+	// Retirar anuncio próprio (perguntar informacao)
+	if(u.ClientStep[name] == 4){
+		u.ClientStep[name] = 8
+		return "Qual o id do veiculo para retirar o anuncio? "
+	}
+	// Comprar carro (perguntar informacao)
+	if(u.ClientStep[name] == 5){
+		u.ClientStep[name] = 9
+		return "Qal o id do veiculo para comprar? "
+	}
+	// Listar carros proprios
+	if(u.ClientStep[name] == 6) {
+		cars := u.WebmotorsApp.ListOwnerCarsToSell(name)
+		response := ""
+		for _, v := range cars {
+			response = response + "\n" + StringfyCar(v)
+		}
+		cars = u.WebmotorsApp.ListBuyerCars(name)
+		for _, v := range cars {
+			response = response + "\n" + StringfyCar(v)
+		}
+		u.ClientStep[name] = 0 // Return to menu step
+		if len(response) == 0 {
+			response = "\nNenhum carro a venda\n"
+		}
+		return response + menuMessage
+	}
+	// Anunciar carro
+	if(u.ClientStep[name] == 7){
+		if numberOfSeparators := strings.Count(message, ";"); numberOfSeparators != 4 {
+			return "### ERRO Dados para carro inválidos ### \n" + menuMessage
+		}
+		data := strings.Split(message,  ";")
+		year, err := strconv.ParseInt(data[2], 10, 16)
+		if err != nil {
+			return "### ERRO Campo ano ###" + menuMessage
+		}
+		km, err := strconv.ParseInt(data[3], 10, 32)
+		if err != nil {
+			return "### ERRO Campo quilometragem ###" + menuMessage
+		}
+		price, err := strconv.ParseFloat(strings.ReplaceAll(data[4], ",", "."), 32)
+		if err != nil {
+			return "### ERRO Campo preço ###" + menuMessage
+		}
+
+		u.WebmotorsApp.AdvertiseCar(&Car{
+			owner: name,
+			buyer: "",
+			model: data[0],
+			color: data[1],
+			year: uint16(year),
+			km: uint32(km),
+			price: float32(price),
+		})
+		u.ClientStep[name] = 0
+		return "### Carro anunciado com sucesso ### \n\n" + menuMessage 
+	}
+	// Retirar anuncio proprio
+	if(u.ClientStep[name] == 8){
+		// Get message
+		code, err := strconv.ParseInt(message, 10, 8)
+		u.ClientStep[name] = 0
+		// User sent an option
+		if err != nil {
+			return fmt.Sprintf("ERRO com id %s\n", err)
+		}
+		fmt.Printf("Removendo carro do id %d\n", code)
+		removed := u.WebmotorsApp.RemoveCarAd(uint32(code))
+		if removed {
+			return "### Removido com sucesso ###\n\n"+ menuMessage
+		}
+		return "### ERRO Id inexistente ### \n\n" + menuMessage
+	}
+	// Comprar carro
+	if(u.ClientStep[name] == 9){
+		// Get message
+		code, err := strconv.ParseInt(message, 10, 8)
+		u.ClientStep[name] = 0
+		// User sent an option
+		if err != nil {
+			return "### ERRO com input ### \n" + menuMessage
+		}
+		removed := u.WebmotorsApp.BuyCar(uint32(code), name)
+		if removed {
+			return "### Comprado com sucesso ###\n\n"+ menuMessage
+		}
+		return "### ERRO Id ou tentativa de compra do proprio carro ### \n\n" + menuMessage
 	}
 	return ""
 }
--- webmotors/webmotors.go
+++ webmotors/webmotors.go
@@ -1,7 +1,8 @@
 package webmotors
 
-import "fmt"
-
+import (
+	"fmt"
+)
 type Car struct {
 	id uint32
 	owner string
@@ -14,45 +15,81 @@ type Car struct {
 }
 
 type Webmotors struct {
-	CarsToSell []*Car
-	CarsSold []*Car
+	Cars []*Car
+	UserInterface *Ui
 }
 
 func NewWebMotos() *Webmotors {
-	w := Webmotors{[]*Car{},[]*Car{}}
+	w := Webmotors{make([]*Car, 0), &Ui{ClientStep: map[string]uint8{}}}
 	return &w
 }
 
 func StringfyCar(c *Car) string{
-	return "Model: " + c.model + "\nColor: " + c.color + "\nYear: " + fmt.Sprint(c.year) + "\nKm: " + fmt.Sprint(c.km) + "\nPrice: " + fmt.Sprintf("%f", c.price) + "\n"
+	return " ### Id: " + fmt.Sprint(c.id) + " ### \nModel: " + c.model + "\nColor: " + c.color + "\nYear: " + fmt.Sprint(c.year) + "\nKm: " + fmt.Sprint(c.km) + "\nPrice: " + fmt.Sprintf("%f", c.price) + "\n"
 }
 
 func (w *Webmotors) ListOwnerCarsToSell(ownerName string) []*Car {
-	response := make([]*Car, 0, len(w.CarsToSell))
-	for i := 0; i < len(w.CarsToSell); i++ {
-		if w.CarsToSell[i].owner == ownerName{
-			response = append(response, w.CarsToSell[i])
+	response := make([]*Car, 0, len(w.Cars))
+	for i := 0; i < len(w.Cars); i++ {
+		if w.Cars[i].buyer == "" && w.Cars[i].owner == ownerName{
+			response = append(response, w.Cars[i])
 		}
 	}
 	return response
 }
 
 func (w *Webmotors) ListOwnerCarsSold(ownerName string) []*Car {
-	response := make([]*Car, 0, len(w.CarsSold))
-	for i := 0; i < len(w.CarsSold); i++ {
-		if w.CarsSold[i].owner == ownerName{
-			response = append(response, w.CarsSold[i])
+	response := make([]*Car, 0, len(w.Cars))
+	for i := 0; i < len(w.Cars); i++ {
+		if w.Cars[i].buyer != "" && w.Cars[i].owner == ownerName{
+			response = append(response, w.Cars[i])
 		}
 	}
 	return response
 }
 
 func (w *Webmotors) ListBuyerCars(buyerName string) []*Car {
-	response := make([]*Car, 0, len(w.CarsSold))
-	for i := 0; i < len(w.CarsSold); i++ {
-		if w.CarsSold[i].buyer == buyerName{
-			response = append(response, w.CarsSold[i])
+	response := make([]*Car, 0, len(w.Cars))
+	for i := 0; i < len(w.Cars); i++ {
+		if w.Cars[i].buyer == buyerName{
+			response = append(response, w.Cars[i])
 		}
 	}
 	return response
+}
+
+func (w *Webmotors) AdvertiseCar(c *Car) {
+	var higherId uint32 = 0
+	for _, v := range w.Cars {
+		if v.id > uint32(higherId){
+			higherId = v.id
+		}
+	}
+	c.id = higherId + 1
+	w.Cars = append(w.Cars, c)
+}
+
+func(w *Webmotors) RemoveCarAd(cardId uint32) bool {
+	removed := false
+	for i, v := range w.Cars {
+		if v.id == cardId {
+			w.Cars = append(w.Cars[:i], w.Cars[i+1:]...)
+			removed = true
+			break
+		}
+	}
+	return removed
+}
+
+func (w *Webmotors) BuyCar(carId uint32, buyerName string) bool {
+	buyed := false
+	for _, v := range w.Cars {
+		if v.id == carId {
+			if v.owner != buyerName {
+				v.buyer = buyerName
+				buyed = true
+			}
+		}
+	}
+	return buyed
 }
